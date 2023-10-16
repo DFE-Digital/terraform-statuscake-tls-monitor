1b62560fcf19921c33e26577b70105dd9ef5b30b
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2023-10-16 11:50:30 +0100

    Moved provider to its own file

---
+++ provider.tf
@@ -0,0 +1,3 @@
+provider "statuscake" {
+  api_token = local.statuscake_api_token
+}
--- statuscake.tf
+++ statuscake.tf
@@ -1,7 +1,3 @@
-provider "statuscake" {
-  api_token = local.statuscake_api_token
-}
-
 resource "statuscake_ssl_check" "main" {
   check_interval = local.statuscake_check_interval # Interval in seconds to test. (12 hours)
   contact_groups = [statuscake_contact_group.monitoring_team[0].id]
